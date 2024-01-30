65f78325243920fbb1b9f18a492b100cb84120b2
musthafa45 <52942581+musthafa45@users.noreply.github.com>
2024-01-30 20:57:30 +0400

    JoyStick Implemented

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,75 +1,84 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.UI;
+using ETouch = UnityEngine.InputSystem.EnhancedTouch;
 
 public class InputManager : MonoBehaviour
 {
     private PlayerInput playerInput;
-    [SerializeField] private Transform joyStickParent;
+    [SerializeField] private Image[] joyStickImages;
+
     private void Awake()
     {
         playerInput = new PlayerInput();
-        joyStickParent.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        ETouch.Touch.onFingerDown += Touch_onFingerDown;
+        ETouch.Touch.onFingerMove += Touch_onFingerMove;
+        ETouch.Touch.onFingerUp += Touch_onFingerUp;
+
+        SetActiveJoyStickImages(false);
+    }
+
+    private void SetActiveJoyStickImages(bool activeSelf)
+    {
+        foreach (Image image in joyStickImages)
+        {
+            image.enabled = activeSelf;
+        }
+    }
+
+    private void Touch_onFingerDown(Finger currentFinger)
+    {
+        Debug.Log("Finger Down");
+        Vector2 touchPosition = currentFinger.currentTouch.screenPosition;
+        if(IsValidTouchPosition(touchPosition))
+        {
+            joyStickImages[0].transform.position = touchPosition;
+            SetActiveJoyStickImages(true);
+        }
+        
+    }
+
+    private bool IsValidTouchPosition(Vector2 touchPosition)
+    {
+        return touchPosition.y < Screen.height / 2f;
+    }
+
+    private void Touch_onFingerMove(Finger obj)
+    {
+        Debug.Log("Finger Move");
+
+    }
+
+    private void Touch_onFingerUp(Finger obj)
+    {
+        Debug.Log("Finger Up");
+        SetActiveJoyStickImages(false);
+
+    }
+
+    private void Update()
+    {
+        //Debug.Log(playerInput.TouchControl.Move.ReadValue<Vector2>());
     }
 
     private void OnEnable()
     {
         playerInput.Enable();
+        EnhancedTouchSupport.Enable();
     }
+
     private void OnDisable()
     {
         playerInput.Disable();
+        EnhancedTouchSupport.Disable();
+
+        ETouch.Touch.onFingerDown -= Touch_onFingerDown;
+        ETouch.Touch.onFingerMove -= Touch_onFingerMove;
+        ETouch.Touch.onFingerUp -= Touch_onFingerUp;
     }
-
-    private void Start()
-    {
-        playerInput.TouchInput.Move.started += (context) => MoveStarted(context);
-        playerInput.TouchInput.Move.canceled += (context) => MoveEnded(context);
-
-        playerInput.TouchInput.TouchPress.started += (context) => TouchPressStarted(context);
-        playerInput.TouchInput.TouchPress.canceled += (context) => TouchPressEnded(context);
-
-    }
-
-    private void TouchPressStarted(UnityEngine.InputSystem.InputAction.CallbackContext context)
-    {
-        Vector2 touchPos = playerInput.TouchInput.TouchPosition.ReadValue<Vector2>();
-        if (IsValidTouchPosition(touchPos))
-        {
-            Debug.Log("Touch pressed Position is Valid");
-            joyStickParent.position = touchPos;
-            joyStickParent.gameObject.SetActive(true);
-        }
-        else
-        {
-            Debug.Log("Touch pressed Position is Not Valid");
-            joyStickParent.gameObject.SetActive(false);
-        }
-    }
-
-    private bool IsValidTouchPosition(Vector2 touchPosition)
-    {
-        return touchPosition.y > Screen.height / 2f == false ;
-    }
-
-    private void TouchPressEnded(UnityEngine.InputSystem.InputAction.CallbackContext context)
-    {
-        Debug.Log("Touch press Ended");
-    }
-
-    private void MoveStarted(UnityEngine.InputSystem.InputAction.CallbackContext context)
-    {
-        Debug.Log("Move Stated "+context.ReadValue<Vector2>());
-    }
-    private void MoveEnded(UnityEngine.InputSystem.InputAction.CallbackContext context)
-    {
-        Debug.Log("Move Ended " + context.ReadValue<Vector2>());
-    }
-
-    //private void Update()
-    //{
-    //    Debug.Log(playerInput.TouchInput.Move.ReadValue<Vector2>());
-    //}
 }
