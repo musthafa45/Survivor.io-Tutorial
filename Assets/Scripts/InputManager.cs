240afd7207cd6d344702ac3e43f2ddb04469bb09
musthafa45 <52942581+musthafa45@users.noreply.github.com>
2024-02-01 21:15:12 +0400

    Added Player Movement And Joystick Clamping

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,11 +6,14 @@ using ETouch = UnityEngine.InputSystem.EnhancedTouch;
 
 public class InputManager : MonoBehaviour
 {
+    public static InputManager Instance { get; private set; }
+
     private PlayerInput playerInput;
     [SerializeField] private Image[] joyStickImages;
-
+    [SerializeField] private float joyStickOffsetBorder;
     private void Awake()
     {
+        Instance = this;
         playerInput = new PlayerInput();
     }
 
@@ -35,10 +38,19 @@ public class InputManager : MonoBehaviour
         Vector2 touchPosition = currentFinger.currentTouch.screenPosition;
         if(IsValidTouchPosition(touchPosition))
         {
-            joyStickImages[0].transform.position = touchPosition;
+            Vector2 clampedTouchPosition = GetClampedJoyStickStartPos(touchPosition);
+            joyStickImages[0].transform.position = clampedTouchPosition;
             SetActiveJoyStickImages(true);
         }
         
+    }
+
+    private Vector2 GetClampedJoyStickStartPos(Vector2 touchPosition)
+    {
+        float touchPosXclamped = Mathf.Clamp(touchPosition.x, joyStickOffsetBorder, Screen.width - joyStickOffsetBorder);
+        float touchPosYclamped = Mathf.Clamp(touchPosition.y, joyStickOffsetBorder, Screen.height / 2 - joyStickOffsetBorder);
+
+        return new Vector2(touchPosXclamped,touchPosYclamped);
     }
 
     private bool IsValidTouchPosition(Vector2 touchPosition)
@@ -54,6 +66,11 @@ public class InputManager : MonoBehaviour
     private void Update()
     {
         //Debug.Log(playerInput.TouchControl.Move.ReadValue<Vector2>());
+    }
+
+    public Vector2 GetJoystickInput()
+    {
+        return playerInput.TouchControl.Move.ReadValue<Vector2>();
     }
 
     private void OnEnable()
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    [SerializeField] private float playerSpeed = 10;
+    private Vector3 playerLocalScale;
+    private void Awake()
+    {
+        playerLocalScale = transform.localScale;
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 playerInputValue = InputManager.Instance.GetJoystickInput();
+        HandleMovement(playerInputValue);
+        HandleFaceRotation(playerInputValue);
+    }
+
+    private void HandleFaceRotation(Vector2 playerInputValue)
+    {
+        if(playerInputValue.x < 0)
+        {
+            // Player Looking left
+            transform.localScale = new Vector3(-playerLocalScale.x,transform.localScale.y,transform.localScale.z) ;
+        }
+        else
+        {
+            // player Looking right.
+            transform.localScale = new Vector3(playerLocalScale.x, transform.localScale.y, transform.localScale.z);
+
+        }
+    }
+
+    private void HandleMovement(Vector2 playerInputValue)
+    {
+        rigidBody2D.velocity = playerInputValue * playerSpeed * 10 * Time.fixedDeltaTime;
+    }
+}
