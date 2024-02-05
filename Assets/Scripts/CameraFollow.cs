e41eec4a5abfb05ca35d7f503abf1eb9f91f8443
Mohammed Musthafa <mohamedmusthafa3679@gmail.com>
2024-02-05 22:46:16 +0400

    Added Camera Move

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,7 +2,20 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private void Start() {
-        
-    }
+   [SerializeField] private Transform playerTransform = null;
+   [SerializeField] private float offsetX;
+   [SerializeField] private float offsetY;
+    private Camera camera;
+   [SerializeField] private float moveSpeed = 10;
+
+   private void Awake() {
+    camera = Camera.main;
+   }
+   
+   private void Update() {
+    transform.position = Vector3.Lerp(transform.position, new Vector3(
+        Mathf.Clamp(playerTransform.position.x + offsetX,-12f,12f),
+        Mathf.Clamp(playerTransform.position.y + offsetY,-12.5f,12.5f),
+    camera.transform.position.z),moveSpeed * Time.deltaTime);
+   }
 }
